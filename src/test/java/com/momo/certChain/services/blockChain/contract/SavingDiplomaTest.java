856dd7b2c6ef9a81f2d677313f30603699887c54
momolakeit <56810449+momolakeit@users.noreply.github.com>
2021-04-28 07:55:45 -0400

    Update SavingDiplomaTest.java

--- src/test/java/com/momo/certChain/services/blockChain/contract/SavingDiplomaTest.java
+++ src/test/java/com/momo/certChain/services/blockChain/contract/SavingDiplomaTest.java
@@ -1,7 +1,9 @@
 package com.momo.certChain.services.blockChain.contract;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.web3j.EVMTest;
+import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.tx.TransactionManager;
 import org.web3j.tx.gas.ContractGasProvider;
@@ -30,5 +32,15 @@ public class SavingDiplomaTest {
         String value =savingDiploma.get("notRandomom").send();
         assertEquals("",value);
     }
-
+    @Test
+    public void savingDiplomaTestDifferentAdressLanceException (Web3j web3j, TransactionManager transactionManager, ContractGasProvider gasProvider) throws Exception {
+        SavingDiploma savingDiploma = SavingDiploma.deploy(web3j,transactionManager,gasProvider).send();
+        SavingDiploma savingDiploma1 = SavingDiploma.load(savingDiploma.getContractAddress(),web3j,getCredentialsFromPrivateKey(),gasProvider);
+        Assertions.assertThrows(Exception.class,()->{
+            savingDiploma1.addCertificate(randString,certificate).send();
+        });
+    }
+    private Credentials getCredentialsFromPrivateKey() {
+        return Credentials.create("eb9c0408f00aa45ef323f847bd293c62d3c1d789e76c8e2575b206dc95ca020a");
+    }
 }
