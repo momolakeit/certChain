d6be9246eff5e7882fcb1af39a1b131148b2fe24
momolakeit <56810449+momolakeit@users.noreply.github.com>
2021-05-07 07:02:39 -0400

    Update SecurityBeanConfig.java

--- src/main/java/com/momo/certChain/configuration/security/SecurityBeanConfig.java
+++ src/main/java/com/momo/certChain/configuration/security/SecurityBeanConfig.java
@@ -1,4 +1,4 @@
-package com.momo.certChain.security;
+package com.momo.certChain.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
