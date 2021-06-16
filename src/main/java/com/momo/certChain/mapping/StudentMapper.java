8f3295298601f3686c9417d2839ffb7be5c27fd2
momolakeit <56810449+momolakeit@users.noreply.github.com>
2021-06-16 08:33:57 -0400

    Update StudentMapper.java

--- src/main/java/com/momo/certChain/mapping/StudentMapper.java
+++ src/main/java/com/momo/certChain/mapping/StudentMapper.java
@@ -8,7 +8,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(uses = {AddressMapper.class,ImageFileMapper.class,InstitutionMapper.class})
+@Mapper(uses = {AddressMapper.class,ImageFileMapper.class,InstitutionMapper.class,CertificationMapper.class})
 public interface StudentMapper {
 
     StudentMapper instance = Mappers.getMapper(StudentMapper.class);
