80d260dc14deee7a7248162704006b61d1cfc4a6
momolakeit <56810449+momolakeit@users.noreply.github.com>
2021-06-19 18:01:03 -0400

    chercher les liens par le id de leur certification

--- src/main/java/com/momo/certChain/repositories/LienRepository.java
+++ src/main/java/com/momo/certChain/repositories/LienRepository.java
@@ -2,6 +2,11 @@ package com.momo.certChain.repositories;
 
 import com.momo.certChain.model.data.Lien;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LienRepository extends JpaRepository<Lien,String> {
+    @Query("SELECT e FROM  Lien e WHERE e.certification.id=?1")
+    List<Lien> findLienByCertificationId(String certId);
 }
---
+++ src/test/java/com/momo/certChain/repositories/LienRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.momo.certChain.repositories;
+
+import com.momo.certChain.model.data.Certification;
+import com.momo.certChain.model.data.Lien;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class LienRepositoryTest {
+
+    @Autowired
+    private LienRepository lienRepository;
+
+    @Autowired
+    private CertificationRepository certificationRepository;
+
+    Certification certification;
+
+    @BeforeEach
+    public void init() {
+        certification = certificationRepository.save(new Certification());
+
+        createLienWithCertification();
+
+        createLienWithCertification();
+
+    }
+
+    @Test
+    public void findAllByCertificationId(){
+        List<Lien> lienList = lienRepository.findLienByCertificationId(certification.getId());
+
+        assertEquals(2,lienList.size());
+    }
+
+    @Test
+    public void findAllByCertificationIdCleanTableLienNoData(){
+        lienRepository.deleteAll();
+
+        List<Lien> lienList = lienRepository.findLienByCertificationId(certification.getId());
+
+        assertEquals(0,lienList.size());
+    }
+
+    @Test
+    public void findAllByCertificationNoLienForData(){
+        List<Lien> lienList = lienRepository.findLienByCertificationId("123456");
+
+        assertEquals(0,lienList.size());
+    }
+
+    private Lien createLienWithCertification() {
+        Lien lien = new Lien();
+        lien.setCertification(certification);
+        lienRepository.save(lien);
+        return lienRepository.save(lien);
+    }
+}
